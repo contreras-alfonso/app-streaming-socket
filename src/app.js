e4053d11a5cb1d1e3237ab6b471ae6b23920a0aa
Alfonso Contreras <contrerasalfonso.lino@gmail.com>
2023-11-15 01:56:02 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -11,13 +11,16 @@ const io = new SocketIOServer(server);
 //rutas
 app.use('/',router);
 //donde vamos a cargar los html con los que vamos a trabajar.
-app.use(express.static('./src/public'))
+// app.use(express.static('./src/public'))
+app.set('view engine','pug'); //especificando el tipo de engine que es pug
+app.set('views','./src/public'); // especificando que carpeta será la de vistas
+
 
 io.on('connection',(socket)=>{
     socket.on('stream',(image)=>{
         //emitir el evento a todos los sockets connectados
         socket.broadcast.emit('stream',image);
-        
+
     })
 })
 
--- src/routes/zoomRoutes.js
+++ src/routes/zoomRoutes.js
@@ -2,8 +2,13 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/index',(req,res)=>{
     res.render('index');
+})
+
+
+router.get('/visualizar',(req,res)=>{
+    res.render('visualizar');
 })
 
 export default router
